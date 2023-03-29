3c3e37ac0b5e69661ced4f3e593366601ba417d3
Tryals <killerzo@yandex.com>
2023-03-29 10:12:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"Text.txt", Encoding.UTF8);
+            Console.WriteLine(sr.ReadToEnd());
+            Console.ReadKey();
+        }
+    }
+}
